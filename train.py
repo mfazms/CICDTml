cada2049e42d84faa125565e4e9211bf598a7c22
mfazms <fazlesidiki@gmail.com>
2025-04-09 04:06:33 +0000

    Update with new results

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-#Install & Import Packages
+# Install & Import Packages
 import pandas as pd
 import matplotlib.pyplot as plt
 import skops.io as sio
@@ -11,12 +11,12 @@ from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 
-#Load Dataset
+# Load Dataset
 drug_df = pd.read_csv("Data/drug.csv")
 drug_df = drug_df.sample(frac=1)
 drug_df.head(3)
 
-#Train Test Split
+# Train Test Split
 X = drug_df.drop("Drug", axis=1).values
 y = drug_df.Drug.values
 
@@ -24,9 +24,9 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=125
 )
 
-#Machine Learning Pipeline
-cat_col = [1,2,3]
-num_col = [0,4]
+# Machine Learning Pipeline
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 
 transform = ColumnTransformer(
     [
@@ -45,22 +45,22 @@ pipe = Pipeline(
 
 pipe.fit(X_train, y_train)
 
-#Model Evaluation
+# Model Evaluation
 predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
 
 print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 
-#Save Metrics
+# Save Metrics
 with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2)}, F1 Score = {round(f1, 2)}.")
-    
-#Create Confusion Metrics
+
+# Create Confusion Metrics
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
 plt.savefig("Results/model_results.png", dpi=120)
 
-#Save Model
+# Save Model
 sio.dump(pipe, "Model/drug_pipeline.skops")
